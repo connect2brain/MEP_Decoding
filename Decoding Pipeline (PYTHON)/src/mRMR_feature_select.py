973aa8b73b13276492d2388a6ef1d0fda713150b
lisahaxel <117662813+lisahaxel@users.noreply.github.com>
2024-11-22 00:44:47 -0800

    Update mRMR_feature_select.py

--- Decoding Pipeline (PYTHON)/src/mRMR_feature_select.py
+++ Decoding Pipeline (PYTHON)/src/mRMR_feature_select.py
@@ -40,9 +40,30 @@ import numpy as np
 from joblib import Parallel, delayed
 from joblib import load, dump
 
-from src.identity_transformer import IdentityTransformer
+from scipy.stats import kendalltau
 
-from src import depmeas	
+def abs_tau(x,y):
+    tau_val,p_val = kendalltau(x,y)
+    if(np.isnan(tau_val)):
+        return 0
+    return np.abs(tau_val)
+
+def mi_tau(x,y):
+    tau_val,p_val = kendalltau(x,y)
+    if(np.isnan(tau_val)):
+        return 0
+    return dep2mi(tau_val)
+    
+def dep2mi(x):
+    MAX_VAL = 0.999999;
+    if(x>MAX_VAL):
+        x = MAX_VAL
+    elif(x<-MAX_VAL):
+        x = -MAX_VAL
+    
+    y = -0.5*np.log(1-x*x);
+
+    return y	
 
 def generic_combined_scorer(x1,o1,ii_1,x2,o2,ii_2,y,h):
     s1 = h(x1,y)
